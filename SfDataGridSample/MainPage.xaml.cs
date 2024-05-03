2846649c5ae0f7b20416ddfabce8118aa545a1e3
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-05-03 16:40:40 +0530

    Added sample and content

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SfDataGridSample.Converter;
+using Syncfusion.Maui.DataGrid;
+using System.Data;
+using System.Reflection;
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Equivalent to the code in the XAMl section
+
+            //DataGridTextColumn salaryColumn = new DataGridTextColumn() { MappingName = "Salary", HeaderText = "Salary" };
+            //salaryColumn.DisplayBinding = new Binding() { Path = "Salary", Converter = new CurrencyFormatConverter() };
+            //dataGrid.Columns.Add(salaryColumn);
+        }
+
+
+    } 
+}
